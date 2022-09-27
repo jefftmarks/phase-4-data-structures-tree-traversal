ce652df2fec67ec21db53bd70a83872224edbae5
Jeff Marks <jeff.t.marks@gmail.com>
2022-09-27 16:48:45 -0400

    commit tree 68c5a73c4ff4d4e9e6235c9da7f5c2556884a987

--- lib/tree.rb
+++ lib/tree.rb
@@ -1,5 +1,57 @@
+require 'pry'
+
 class Tree
+	attr_accessor :root
+
   def initialize(root = nil)
     @root = root
   end
+
+	def depth_first_traversal(node)
+		result = []
+		nodes_to_visit = [node]
+	
+		while nodes_to_visit.length > 0
+			node = nodes_to_visit.shift
+			result.push(node[:value])
+			nodes_to_visit = node[:children] + nodes_to_visit
+		end
+	
+		result
+	end
+
+	# def get_element_by_id(str)
+	# 	result = []
+	# 	nodes_to_visit = [self.root]
+	
+	# 	while nodes_to_visit.length > 0
+	# 		node = nodes_to_visit.shift
+	# 		if node[:id] == str
+	# 			return node
+	# 		else
+	# 			result.push(node[:value])
+	# 			nodes_to_visit = node[:children] + nodes_to_visit
+	# 		end
+	# 	end
+	
+	# 	nil
+	# end
+
+	def get_element_by_id(str)
+		nodes_to_visit = [self.root]
+	
+		while nodes_to_visit.length > 0
+			node = nodes_to_visit.shift
+			if node[:id] == str
+				return node
+			else
+				nodes_to_visit = nodes_to_visit + node[:children]
+			end
+		end
+	
+		nil
+	end
+
 end
+
+# binding.pry
